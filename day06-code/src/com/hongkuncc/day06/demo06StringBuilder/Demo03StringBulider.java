1ca9a348ca4b1586681635d71cfefc83d2f1eeed
hongkuncc <hongkuncc@163.com>
2020-04-29 00:06:23 +0800

    initial

--- day06-code/src/com/hongkuncc/day06/demo06StringBuilder/Demo03StringBulider.java
+++ day06-code/src/com/hongkuncc/day06/demo06StringBuilder/Demo03StringBulider.java
@@ -2,6 +2,15 @@ package com.hongkuncc.day06.demo06StringBuilder;
 
 public class Demo03StringBulider {
     public static void main(String[] args) {
+        String str = "hello";
+        System.out.println("str"+str);
+        StringBuilder bu = new StringBuilder(str);
+        //往StringBuilder中添加数据
+        bu.append("world");
+        System.out.println("bu:"+bu);
+
+        String s = bu.toString();
+        System.out.println("s:"+s);
 
     }
 }
