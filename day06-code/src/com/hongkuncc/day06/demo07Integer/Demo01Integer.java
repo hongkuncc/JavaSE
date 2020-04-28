99655b49dda53d984bebf9b8ccc53adfb31c8b3f
hongkuncc <hongkuncc@163.com>
2020-04-29 00:15:57 +0800

    initial

---
+++ day06-code/src/com/hongkuncc/day06/demo07Integer/Demo01Integer.java
@@ -0,0 +1,15 @@
+package com.hongkuncc.day06.demo07Integer;
+/*
+*
+*java.lang.Integer
+*
+* 装箱：把基本类型的数据，包装到包装类中（基本类型的数据->包装类）
+*
+* 构造方法：
+* Integer(int value)构造一个新分配的 Integer 对象，它表示指定的 int 值。
+  Integer(String s)构造一个新分配的 Integer 对象，它表示 String 参数所指示的 int 值。
+* */
+public class Demo01Integer {
+
+}
+
