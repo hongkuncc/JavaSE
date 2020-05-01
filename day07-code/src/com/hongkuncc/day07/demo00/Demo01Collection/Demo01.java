887481ce28e6d37d56aa475e1d441c4b16fcfab3
hongkuncc <hongkuncc@163.com>
2020-05-02 00:15:34 +0800

    initial

--- day07-code/src/com/hongkuncc/day07/demo00/Demo01Collection/Demo01.java
+++ day07-code/src/com/hongkuncc/day07/demo00/Demo01Collection/Demo01.java
@@ -23,6 +23,52 @@ public class Demo01 {
     public static void main(String[] args) {
 //        创建集合对象可以使用多态
         Collection<String> coll = new ArrayList<>();
+        System.out.println(coll);//重写toString方法
+
+        boolean b1 = coll.add("张三");
+        System.out.println("b1:"+b1);
+        System.out.println(coll);
+        coll.add("李四");
+        coll.add("王五");
+        coll.add("赵六");
+        coll.add("田七");
+        System.out.println(coll);
+
+        // boolean remove(Object o)
+        boolean b2 = coll.remove("赵六");
+        System.out.println("b2:"+b2);
+
+        boolean b3 = coll.remove("赵四");
+        System.out.println("b3:"+b3);
+        System.out.println(coll);
+
+        //boolean contains(Object o)
+
+        boolean b4 = coll.contains("李四");
+        System.out.println("b4:"+b4);
+
+        boolean b5 = coll.contains("赵四");
+        System.out.println("b5:"+b5);
+
+        //boolean isEmpty()
+        boolean b6 = coll.isEmpty();
+        System.out.println("b6:"+b6);
+
+       // int size() 返回此 collection 中的元素数
+        int size = coll.size();
+        System.out.println("size:"+size);
+
+        //Object[] toArray()
+        Object[] arr = coll.toArray();
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println(arr[i]);
+        }
+
+        //void clear()
+        coll.clear();
+        System.out.println(coll);
+        System.out.println(coll.isEmpty());
+
     }
 
 }
