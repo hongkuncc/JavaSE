b2b890acc6fb744eb98e8837c260e3e57b3bacc7
hongkuncc <hongkuncc@163.com>
2020-05-03 01:05:44 +0800

    initial

---
+++ day11-code/src/com/hongkuncc/day11/demo01/MyFunctionalInterface.java
@@ -0,0 +1,12 @@
+package com.hongkuncc.day11.demo01;
+/*
+* @FunctionalInterface注解，可以检测接口是否是一个函数式接口
+* */
+@FunctionalInterface
+
+public interface MyFunctionalInterface {
+    //定义一个抽象方法
+    public abstract void method();
+
+
+}
---
+++ day11-code/src/com/hongkuncc/day11/demo01/MyFunctionalInterfaceImpl.java
@@ -0,0 +1,14 @@
+package com.hongkuncc.day11.demo01;
+
+public class MyFunctionalInterfaceImpl implements MyFunctionalInterface{
+
+    @Override
+    public void method() {
+
+    }
+
+   /* @Override
+    public void method2() {
+
+    }*/
+}
