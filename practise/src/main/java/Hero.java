c96f88e3e09d0d7db1a184af748f9653bd06b04e
hongkuncc <hongkuncc@163.com>
2020-07-12 20:20:08 +0800

    initial

---
+++ practise/src/main/java/Hero.java
@@ -0,0 +1,21 @@
+public class Hero {
+    String name;
+    float hp;
+    float armor;
+    int moveSpeed;
+
+    public static void main(String[] args) {
+        Hero garen = new Hero();
+        garen.name="盖伦";
+        garen.hp=616.28f;
+        garen.armor=27.536f;
+        garen.moveSpeed=330;
+
+        Hero teemo = new Hero();
+        garen.name="提莫";
+        garen.hp=383f;
+        garen.armor=14f;
+        garen.moveSpeed=330;
+    }
+
+}
---
+++ practise/src/main/java/Item.java
@@ -0,0 +1,18 @@
+public class Item {
+    String name;
+    int price;
+
+    public static void main(String[] args) {
+        Item item1 = new Item();
+        Item item2 = new Item();
+        Item item3 = new Item();
+        item1.name="血瓶";
+        item2.name="草鞋";
+        item3.name="长剑";
+        item1.price=50;
+        item2.price=300;
+        item3.price=350;
+
+    }
+
+}
