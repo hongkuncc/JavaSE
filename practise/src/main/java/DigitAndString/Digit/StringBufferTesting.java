2f936ef32b0a2441684ca03a670a6635ea6bad7e
hongkuncc <hongkuncc@163.com>
2020-11-25 00:33:13 +0800

    StringBuffer

--- practise/src/main/java/DigitAndString/Digit/StringBufferTesting.java
+++ practise/src/main/java/DigitAndString/Digit/StringBufferTesting.java
@@ -22,19 +22,38 @@ public class StringBufferTesting {
 
         System.out.println(sb.length());//内容长度
         System.out.println(sb.capacity());//总空间
-        long currentDate = System.currentTimeMillis();
+
+        int total = 10000;
+        String s = randomString(10);
+        StringBuffer ss = new StringBuffer();
+
+        String str2 = "";
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < total; i++) {
+            str2 += s;
+        }
+        long end = System.currentTimeMillis();
+        System.out.printf("使用字符串连接+的方式，连接%d次，耗时%d毫秒%n",total,end-start);
+        total *= 100;
+        start = System.currentTimeMillis();
+        for (int i = 0; i < total; i++) {
+            ss.append(s);
+        }
+        end = System.currentTimeMillis();
+        System.out.printf("使用字符串连接+的方式，连接%d次，耗时%d毫秒%n",total,end-start);
 
     }
     private static String randomString(int length){
-        String pool ="";
+        String pool = "";
         for (int i = '0'; i <'9' ; i++) {
-            pool +=(char)i;
+            pool += (char)i;
         }
         for (int i = 'a'; i <'z' ; i++) {
-            pool +=(char)i;
+            pool += (char)i;
         }
         for (int i = 'A'; i < 'Z'; i++) {
-            pool +=(char)i;
+            pool += (char)i;
         }
         char cs[] = new char[length];
         for (int i = 0; i <cs.length ; i++) {
