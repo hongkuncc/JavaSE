235dd2fa4cd196473d3fdaa550578ab18800f9d0
hongkuncc <hongkuncc@163.com>
2020-11-25 23:53:49 +0800

    StringBuffer

--- practise/src/main/java/DigitAndString/Digit/MyStringBuffer.java
+++ practise/src/main/java/DigitAndString/Digit/MyStringBuffer.java
@@ -14,14 +14,30 @@ public class MyStringBuffer implements IStringBuffer{
     }
 
     //有参构造方法
-    public MyStringBuffer(String str) {
+/*    public MyStringBuffer(String str) {
         if (null != str)
             value = str.toCharArray();
 
         length = value.length;
         if (capacity < value.length)
             capacity = value.length * 2;
+    }*/
+
+    //有参构造方法
+    public MyStringBuffer(String str) {
+        this();
+        if (null == str)
+            return;
+
+        if (capacity < str.length()) {
+            capacity = value.length * 2;
+            value = new char[capacity];
+        }
+        if (capacity >= str.length())
+            System.arraycopy(str.toCharArray(),0,value,0,str.length());
+        length = str.length();
     }
+
 
     @Override
     public void append(String str) {
@@ -40,6 +56,31 @@ public class MyStringBuffer implements IStringBuffer{
 
     @Override
     public void insert(int pos, String b) {
+        //边界条件判断
+        if (pos < 0)
+            return;
+        if (pos > length)
+            return;
+        if (null == b)
+            return;
+
+        //扩容
+        while(length+b.length()>capacity){
+            capacity = (int)((length+b.length())*1.5f);
+            char[] newValue = new char[capacity];
+            System.arraycopy(value,0,newValue,0,length);
+            value = newValue;
+        }
+
+        char[] cs = b.toCharArray();
+
+        //先把已经存在的数据往后移
+
+        System.arraycopy(value, pos, value,pos+ cs.length, length-pos);
+        //把要插入的数据插入到指定位置
+        System.arraycopy(cs, 0, value, pos, cs.length);
+
+        length = length+cs.length;
 
     }
 
@@ -55,11 +96,16 @@ public class MyStringBuffer implements IStringBuffer{
 
     @Override
     public void reverse() {
+        for (int i = 0; i <length/2 ; i++) {
+            char temp = value[i];
+            value[i] = value[length - i - 1];
+            value[length -i - 1] =temp;
+        }
 
     }
 
     @Override
     public int length() {
-        return 0;
+        return length;
     }
 }
