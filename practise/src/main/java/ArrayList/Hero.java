3e1e09aec4428fff78652818a671c07819ed15bc
hongkuncc <hongkuncc@163.com>
2020-12-14 00:19:55 +0800

    ArrayList

---
+++ practise/src/main/java/ArrayList/Hero.java
@@ -0,0 +1,14 @@
+package ArrayList;
+
+
+
+public class Hero {
+    String name;
+    float hp;
+    float armor;
+    int moveSpeed;
+    public Hero(String name){
+        this.name = name;
+    }
+
+}
